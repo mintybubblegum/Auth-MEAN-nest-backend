00e5004e822360cd54baaa04c212261a2c90ac9c
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-07-28 15:26:04 +0200

    encripting password - exercise 342

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import *as bcryptjs from 'bcryptjs';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -18,8 +20,18 @@ export class AuthService {
     
 
     try {
-      const newUser = new this.userModel( createUserDto );
-      return await newUser.save();
+
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10 ),
+        ...userData
+      });
+
+        await newUser.save();
+        const { password:_, ...user } = newUser.toJSON();
+
+        return user;
 
     } catch (error) {
       if( error.code === 11000 ) {
