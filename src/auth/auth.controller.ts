04d68c859aae63b781da5d63aa04dcd8df1192e2
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-07-31 15:06:48 +0200

    new token generation - exercise 348

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
+
+
 
 @Controller('auth')
 export class AuthController {
@@ -26,13 +30,27 @@ export class AuthController {
   @UseGuards( AuthGuard )
   @Get()
   findAll( @Request() req: Request ) {
-    const user = req['user'];
+    //const user = req['user'];
 
-    return user;
-    //return this.authService.findAll();
+    //return user;
+    return this.authService.findAll();
   }
 
-  @Get(':id')
+  //LoginResponse
+  @UseGuards( AuthGuard )
+  @Get('check-token')
+  checkToken( @Request() req: Request ): LoginResponse {
+
+    const user = req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwtToken({ id: user._id })
+    }
+    
+  }
+
+  /* @Get(':id')
   findOne(@Param('id') id: string) {
     return this.authService.findOne(+id);
   }
@@ -45,5 +63,5 @@ export class AuthController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
-  }
+  } */
 }
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     const token = this.extractTokenFromHeader(request);
     
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('There is no bearer token');
     }
 
     try {
