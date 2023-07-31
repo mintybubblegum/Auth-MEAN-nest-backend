b002c5cb9c6fe3c1af9d41b67154451a9e64589c
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-07-31 14:04:30 +0200

    protect routes with authorization - exercise 346

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -83,8 +83,8 @@ if ( !user ) {
     */
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
   }
 
   findOne(id: number) {
